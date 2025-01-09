5bde0498761acd75b1c623da8efb27d5e7dd2024
sachinpandeygurh <sachingurh@gmail.com>
2025-01-09 17:30:26 +0530

    prfile update

--- src/layouts/ProfileLayout.tsx
+++ src/layouts/ProfileLayout.tsx
@@ -210,18 +210,17 @@ const {user} = useAuthContext();
         if (!response.ok) {
           throw new Error('Network response was not ok');
         }
-
         const data = await response.json(); 
-        setProfile(data.data); 
+        setProfile(data?.data); 
       } catch (error) {
         console.error("Error fetching user profile:", error);
       }
     };
-    if (profile?.coverimurl){
+    if (profile?.coverimurl && profile?.personalDetails){
       return;
     }
     fetchUser();
-  }, [profile.personalDetails]); 
+  }, [profile?.personalDetails]); 
   
   const formatDate = (dateString) => {
     const date = new Date(dateString);
@@ -244,156 +243,165 @@ const {user} = useAuthContext();
         <TopHeader />
       </Suspense>
 
-     {profile?.coverimurl&&( <main>
-        <Container>
-          <Row className="g-4">
-            <Col lg={8} className="vstack gap-4">
-              <Card>
-                <div
-                  className="h-200px rounded-top"
-                  style={{
-                    backgroundImage: `url(${profile.coverimurl?profile.coverimurl:background5})`,
-                    backgroundPosition: 'center',
-                    backgroundSize: 'cover',
-                    backgroundRepeat: 'no-repeat',
-                  }}
-                />
-                <CardBody className="py-0">
-                  <div className="d-sm-flex align-items-start text-center text-sm-start">
-                    <div>
-                      <div className="avatar avatar-xxl mt-n5 mb-3">
-                        <img className="avatar-img rounded-circle border border-white border-3" src={profile.profileimgurl?profile.profileimgurl:avatar7} alt="avatar" />
-                      </div>
-                    </div>
-                    <div className="ms-sm-4 mt-sm-3">
-                      <h1 className="mb-0 h5">
-                      {profile?.personalDetails?.firstName} {profile?.personalDetails?.lastName} <BsPatchCheckFill className="text-success small" />
-                      </h1>
-                      <p>{profile.personalDetails.bio}</p>
-                    </div>
-                    <div className="d-flex mt-3 justify-content-center ms-sm-auto">
-                      <Button variant="danger-soft" className="me-2" type="button">
-                        
-                        <BsPencilFill size={19} className="pe-1" /> Edit profile
-                      </Button>
-                      <Dropdown>
-                        <DropdownToggle
-                          as="a"
-                          className="icon-md btn btn-light content-none"
-                          type="button"
-                          id="profileAction2"
-                          data-bs-toggle="dropdown"
-                          aria-expanded="false">
-                          <span>
-                            
-                            <BsThreeDots />
-                          </span>
-                        </DropdownToggle>
-                        <DropdownMenu className="dropdown-menu-end" aria-labelledby="profileAction2">
-                          <li>
-                            <DropdownItem>
-                              
-                              <BsBookmark size={22} className="fa-fw pe-2" />
-                              Share profile in a message
-                            </DropdownItem>
-                          </li>
-                          <li>
-                            <DropdownItem>
-                              
-                              <BsFileEarmarkPdf size={22} className="fa-fw pe-2" />
-                              Save your profile to PDF
-                            </DropdownItem>
-                          </li>
-                          <li>
-                            <DropdownItem>
-                              
-                              <BsLock size={22} className="fa-fw pe-2" />
-                              Lock profile
-                            </DropdownItem>
-                          </li>
-                          <li>
-                            <hr className="dropdown-divider" />
-                          </li>
-                          <li>
-                            <DropdownItem>
-                              
-                              <BsGear size={22} className="fa-fw pe-2" />
-                              Profile settings
-                            </DropdownItem>
-                          </li>
-                        </DropdownMenu>
-                      </Dropdown>
-                    </div>
+      {profile?.profileimgurl && profile?.personalDetails && (
+  
+ <main>
+    <Container>
+      <Row className="g-4">
+        {/* Main Profile Section */}
+        <Col lg={8} className="vstack gap-4">
+        <Card>
+            {/* Profile Cover Image */}
+            <div
+              className="h-200px rounded-top"
+              style={{
+                backgroundImage: `url(${profile?.coverimurl ? profile?.coverimurl : background5})`,
+                backgroundPosition: 'center',
+                backgroundSize: 'cover',
+                backgroundRepeat: 'no-repeat',
+              }}
+            />
+            <CardBody className="py-0">
+              {/* Profile Info Section */}
+              <div className="d-sm-flex align-items-start text-center text-sm-start">
+                {/* Profile Picture */}
+                <div>
+                  <div className="avatar avatar-xxl mt-n5 mb-3">
+                    <img
+                      className="avatar-img rounded-circle border border-white border-3"
+                      src={profile.profileimgurl ? profile.profileimgurl : avatar7}
+                      alt="avatar"
+                    />
                   </div>
-                  <ul className="list-inline mb-0 text-center text-sm-start mt-3 mt-sm-0">
-                    <li className="list-inline-item">
-                      <BsBriefcase className="me-1" /> {profile?.personalDetails?.occupation}
+                </div>
+                {/* Name and Bio */}
+                <div className="ms-sm-4 mt-sm-3">
+                  <h1 className="mb-0 h5">
+                    {profile?.personalDetails?.firstName} {profile?.personalDetails?.lastName}{' '}
+                    <BsPatchCheckFill className="text-success small" />
+                  </h1>
+                  <p>{profile.personalDetails?.bio}</p>
+                </div>
+                {/* Action Buttons */}
+                <div className="d-flex mt-3 justify-content-center ms-sm-auto">
+                  <Button variant="danger-soft" className="me-2" type="button">
+                    <BsPencilFill size={19} className="pe-1" /> Edit profile
+                  </Button>
+                  <Dropdown>
+                    <DropdownToggle
+                      as="a"
+                      className="icon-md btn btn-light content-none"
+                      id="profileAction2"
+                      data-bs-toggle="dropdown"
+                      aria-expanded="false"
+                    >
+                      <BsThreeDots />
+                    </DropdownToggle>
+                    <DropdownMenu className="dropdown-menu-end" aria-labelledby="profileAction2">
+                      <DropdownItem>
+                        <BsBookmark size={22} className="fa-fw pe-2" /> Share profile in a message
+                      </DropdownItem>
+                      <DropdownItem>
+                        <BsFileEarmarkPdf size={22} className="fa-fw pe-2" /> Save your profile to PDF
+                      </DropdownItem>
+                      <DropdownItem>
+                        <BsLock size={22} className="fa-fw pe-2" /> Lock profile
+                      </DropdownItem>
+                      <hr className="dropdown-divider" />
+                      <DropdownItem>
+                        <BsGear size={22} className="fa-fw pe-2" /> Profile settings
+                      </DropdownItem>
+                    </DropdownMenu>
+                  </Dropdown>
+                </div>
+              </div>
+              {/* Profile Details */}
+              <ul className="list-inline mb-0 text-center text-sm-start mt-3 mt-sm-0">
+                <li className="list-inline-item">
+                  <BsBriefcase className="me-1" /> {profile?.personalDetails?.occupation}
+                </li>
+                <li className="list-inline-item">
+                  <BsGeoAlt className="me-1" />{' '}
+                  {profile?.personalDetails?.permanentAddress?.city}{' '}
+                  {profile?.personalDetails?.permanentAddress?.state}
+                </li>
+                <li className="list-inline-item">
+                  <BsCalendar2Plus className="me-1" /> Joined on{' '}
+                  {profile?.personalDetails?.createdAt &&
+                    formatDate(profile.personalDetails?.createdAt)}
+                </li>
+              </ul>
+            </CardBody>
+            <CardFooter className="card-footer mt-3 pt-2 pb-0">
+              <ul className="nav nav-bottom-line align-items-center justify-content-center justify-content-md-start mb-0 border-0">
+                {PROFILE_MENU_ITEMS.map((item, idx) => (
+                  <li className="nav-item" key={idx}>
+                    <Link
+                      className={clsx('nav-link', { active: pathname === item.url })}
+                      to={item.url ?? ''}
+                    >
+                      {item.label}{' '}
+                      {item.badge && (
+                        <span className="badge bg-success bg-opacity-10 text-success small">
+                          {item.badge.text}
+                        </span>
+                      )}
+                    </Link>
+                  </li>
+                ))}
+              </ul>
+            </CardFooter>
+          </Card>
+          {/* <Suspense fallback={<FallbackLoading />}>{children}</Suspense> */}
+        </Col>
+
+        {/* Sidebar Section */}
+        <Col lg={4}>
+          <Row className="g-4">
+            {/* About Card */}
+            <Col md={6} lg={12}>
+              <Card>
+                <CardHeader className="border-0 pb-0">
+                  <CardTitle>About</CardTitle>
+                </CardHeader>
+                <CardBody className="position-relative pt-0">
+                  <p>
+                    He moonlights difficult engrossed it, sportsmen. Interested has all Devonshire
+                    difficulty gay assistance joy.
+                  </p>
+                  <ul className="list-unstyled mt-3 mb-0">
+                    <li className="mb-2">
+                      <BsCalendarDate size={18} className="fa-fw pe-1" /> Born:{' '}
+                      <strong>October 20, 1990</strong>
                     </li>
-                    <li className="list-inline-item">
-                      <BsGeoAlt className="me-1" /> {profile?.personalDetails?.permanentAddress?.city} {profile?.personalDetails?.permanentAddress?.state}
+                    <li className="mb-2">
+                      <BsHeart size={18} className="fa-fw pe-1" /> Status: <strong>Single</strong>
                     </li>
-                    <li className="list-inline-item">
-                      <BsCalendar2Plus className="me-1" /> Joined on {profile?.personalDetails?.createdAt && formatDate(profile.personalDetails?.createdAt)}
-   
+                    <li>
+                      <BsEnvelope size={18} className="fa-fw pe-1" /> Email:{' '}
+                      <strong>businessroom@ai.com</strong>
                     </li>
                   </ul>
                 </CardBody>
-                <CardFooter className="card-footer mt-3 pt-2 pb-0">
-                  <ul className="nav nav-bottom-line align-items-center justify-content-center justify-content-md-start mb-0 border-0">
-                    {PROFILE_MENU_ITEMS.map((item, idx) => (
-                      <li className="nav-item" key={idx}>
-                        
-                        <Link className={clsx('nav-link', { active: pathname === item.url })} to={item.url ?? ''}>
-                          
-                          {item.label} {item.badge && <span className="badge bg-success bg-opacity-10 text-success small"> {item.badge.text}</span>}
-                        </Link>
-                      </li>
-                    ))}
-                  </ul>
-                </CardFooter>
               </Card>
-              <Suspense fallback={<FallbackLoading/>}> {children}</Suspense>
             </Col>
-            <Col lg={4}>
-              <Row className="g-4">
-                <Col md={6} lg={12}>
-                  <Card>
-                    <CardHeader className="border-0 pb-0">
-                      <CardTitle>About</CardTitle>
-                    </CardHeader>
-                    <CardBody className="position-relative pt-0">
-                      <p>He moonlights difficult engrossed it, sportsmen. Interested has all Devonshire difficulty gay assistance joy.</p>
-                      <ul className="list-unstyled mt-3 mb-0">
-                        <li className="mb-2">
-                          
-                          <BsCalendarDate size={18} className="fa-fw pe-1" /> Born: <strong> October 20, 1990 </strong>
-                        </li>
-                        <li className="mb-2">
-                          
-                          <BsHeart size={18} className="fa-fw pe-1" /> Status: <strong> Single </strong>
-                        </li>
-                        <li>
-                          
-                          <BsEnvelope size={18} className="fa-fw pe-1" /> Email: <strong> webestica@gmail.com </strong>
-                        </li>
-                      </ul>
-                    </CardBody>
-                  </Card>
-                </Col>
-                <Col md={6} lg={12}>
-                  <Experience />
-                </Col>
-                <Col md={6} lg={12}>
-                  <Photos />
-                </Col>
-                <Col md={6} lg={12}>
-                  <Friends />
-                </Col>
-              </Row>
+            {/* Additional Components */}
+            <Col md={6} lg={12}>
+              <Experience />
+            </Col>
+            <Col md={6} lg={12}>
+              <Photos />
+            </Col>
+            <Col md={6} lg={12}>
+              <Friends />
             </Col>
           </Row>
-        </Container>
-      </main>)}
+        </Col>
+      </Row>
+    </Container>
+    </main>
+)}
+
     </>
   )
 }
