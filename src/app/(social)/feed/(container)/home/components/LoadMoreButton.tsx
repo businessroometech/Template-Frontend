aeffb477bd4cbc40a50c87203dd4445aa146ea06
sachinpandeygurh <sachingurh@gmail.com>
2025-01-14 21:56:23 +0530

    fix

--- src/app/(social)/feed/(container)/home/components/LoadMoreButton.tsx
+++ src/app/(social)/feed/(container)/home/components/LoadMoreButton.tsx
@@ -1,22 +1,27 @@
-import clsx from 'clsx';
-import { Button } from 'react-bootstrap';
 
-const LoadMoreButton = ({ isSpinning }) => {
+import useToggle from '@/hooks/useToggle'
+import clsx from 'clsx'
+import { Button } from 'react-bootstrap'
+
+const LoadMoreButton = ({limit,setLimit,isSpinning}) => {
   return (
     <Button
+      onClick={() => {
+        setLimit(limit+5);
+        console.log('-----limit-------',limit);
+      }}
       variant="primary-soft"
       role="button"
       className={clsx('btn-loader', { active: isSpinning })}
       data-bs-toggle="button"
       aria-pressed="true">
-      <span className="load-text">Load more</span>
+      <span className="load-text"> Load more </span>
       <div className="load-icon">
         <div className="spinner-grow spinner-grow-sm" role="status">
           <span className="visually-hidden">Loading...</span>
         </div>
       </div>
     </Button>
-  );
-};
-
-export default LoadMoreButton;
+  )
+}
+export default LoadMoreButton
