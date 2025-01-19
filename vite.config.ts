cb6c7c9f5a342cc67413a23048c6dbb5920e0bee
adkood <ashutoshnegi195@gmail.com>
2025-01-19 20:48:15 +0530

    reverted back

--- vite.config.ts
+++ vite.config.ts
@@ -1,41 +1,13 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
-import { resolve } from 'path';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+import { resolve } from 'path'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [
-    react({
-      // React plugin options
-    }),
-  ],
+  plugins: [react()],
   resolve: {
     alias: {
-      '@': resolve(__dirname, 'src'), // Alias for the `src` directory
-      components: resolve(__dirname, 'src/components'), // Example: Alias for components
-      utils: resolve(__dirname, 'src/utils'), // Example: Alias for utilities
+      "@": resolve(__dirname, "src"),
     },
   },
-  build: {
-    sourcemap: true, // Enable source maps for easier debugging
-    rollupOptions: {
-      output: {
-        manualChunks: {
-          react: ['react', 'react-dom'], // Separate React libraries into their own chunk
-        },
-      },
-    },
-    chunkSizeWarningLimit: 500, // Adjust chunk size warning limit (default is 500KB)
-  }
-  // server: {
-  //   port: 3000, // Specify the development server port
-  //   open: true, // Automatically open the browser on server start
-  //   proxy: {
-  //     '/api': {
-  //       target: 'http://localhost:5000', // Proxy API requests to the backend server
-  //       changeOrigin: true,
-  //       rewrite: (path) => path.replace(/^\/api/, ''),
-  //     },
-  //   },
-  // },
-});
+})
