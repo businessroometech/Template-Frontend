c8f28762e2fdaf5958fd20e6cda07eea5ad4ff2e
sachinpandeygurh <sachingurh@gmail.com>
2025-02-06 00:34:47 +0530

    increase height of YouTube iframe in PostCard component

--- src/components/cards/PostCard.tsx
+++ src/components/cards/PostCard.tsx
@@ -529,7 +529,7 @@ const PostCard = ({
           <iframe
             key={index}
             width="100%"
-            height="250"
+            height="330px"
             src={`https://www.youtube.com/embed/${videoId}`}
             frameBorder="0"
             allow="autoplay; encrypted-media"
