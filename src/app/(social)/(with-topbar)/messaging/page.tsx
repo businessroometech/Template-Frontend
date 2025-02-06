7047edf8b51d1371c3eba84a2a289d44cc601226
vaxxnsh <kakshit817@gmail.com>
2025-02-06 19:11:51 +0530

    ...

--- src/app/(social)/(with-topbar)/messaging/page.tsx
+++ src/app/(social)/(with-topbar)/messaging/page.tsx
@@ -11,14 +11,14 @@ const Messaging = () => {
   return (
     <>
     <PageMetaData title='Messaging'/>
-    <main style={{paddingLeft : '5vw'}}>
+    <main style={{paddingLeft : '5vw',height : '100vh',backgroundColor : 'white'}}>
       <div style={{}} >
         <Row className="gx-0">
           <Col lg={4} xxl={3}>
             <div className="d-flex align-items-center mb-4 d-lg-none" >
               <ChatToggler />
           </div>
-            <Card className="card-body "  >
+            <Card className="card-body" style={{marginTop : '80px'}}>
               <div className=" d-flex justify-content-between align-items-center">
                 <h1 className="h5 mb-0">
                   Active chats <span className="badge bg-success bg-opacity-10 text-success">{totalChats}</span>
